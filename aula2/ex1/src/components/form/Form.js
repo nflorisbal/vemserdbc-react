0db2ba22e6248821b2dec095b18a11bb788c84e1
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-23 10:30:15 -0300

    add useState textarea

--- aula2/ex1/src/components/form/Form.js
+++ aula2/ex1/src/components/form/Form.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import PageTitle from '../pagetitle/PageTitle';
 import Input from '../input/Input';
 import Label from '../label/Label';
@@ -7,32 +7,34 @@ import TextArea from '../textarea/TextArea';
 import style from './Form.module.css';
 
 const Form = () => {
-  
+
   const RegisterUser = (e) => {
     e.preventDefault();
-    console.log(`${username}`);
-    console.log(`${email}`);
+    console.log(`Username: ${username}`);
+    console.log(`E-mail: ${email}`);
+    console.log(`Msg: ${msg}`);
     console.log('Message sent!');
   }
-  
+
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
+  const [msg, setMsg] = useState('');
 
   return (
     <section className={style.container}>
-      <PageTitle pageTitle='Contact Us'/>
+      <PageTitle pageTitle='Contact Us' />
       <form className={style.form} onSubmit={RegisterUser}>
-        <Label forInput='username' textLabel='Username:'/>
-        <Input type='text' name='username' placeholder='Type your username' action={setUsername}/>
+        <Label forInput='username' textLabel='Username:' />
+        <Input type='text' name='username' placeholder='Type your username' action={setUsername} />
         <Label forInput='email' textLabel='E-mail:' />
-        <Input type='text' name='email' placeholder='Type your email' action={setEmail}/>
+        <Input type='text' name='email' placeholder='Type your email' action={setEmail} />
         <Label forInput='questions' textLabel='What do you need?' />
-        <Select name='questions' id='questions'/>
+        <Select name='questions' id='questions' />
         <Label forInput='msg' textLabel='Your message:' />
-        <TextArea />
-        <Input type='submit'/>
+        <TextArea action={setMsg} />
+        <Input type='submit' />
       </form>
-  </section>
+    </section>
   );
 }
 
--- aula2/ex1/src/components/textarea/TextArea.js
+++ aula2/ex1/src/components/textarea/TextArea.js
@@ -1,6 +1,6 @@
-const TextArea = () => {
+const TextArea = ({action}) => {
   return (
-    <textarea name="msg" cols="30" rows="10" placeholder="Type your message."/>
+    <textarea name="msg" cols="30" rows="10" placeholder="Type your message." onChange={(e) => action(e.target.value)}/>
   );
 }
 
