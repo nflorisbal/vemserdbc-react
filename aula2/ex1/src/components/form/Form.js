1c30803d4d7f980951d0ce4d62d3c7f4be2e90ca
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-03-23 08:16:20 -0300

    add title form

--- aula2/ex1/src/components/form/Form.js
+++ aula2/ex1/src/components/form/Form.js
@@ -1,3 +1,4 @@
+import PageTitle from '../pagetitle/PageTitle'
 import Input from '../input/Input';
 import Label from '../label/Label';
 import Select from '../select/Select';
@@ -6,17 +7,20 @@ import style from './Form.module.css'
 
 const Form = () => {
   return (
-    <form className={style.form}>
-      <Label forInput='username' textLabel='Username:' />
-      <Input type='text' name='username' placeholder='Type your username' />
-      <Label forInput='email' textLabel='E-mail:' />
-      <Input type='text' name='email' placeholder='Type your email' />
-      <Label forInput='questions' textLabel='What do you need?' />
-      <Select name='questions' id='questions'/>
-      <Label forInput='msg' textLabel='Your message:' />
-      <TextArea />
-      <Input type='submit'/>
-    </form>
+    <>
+      <PageTitle pageTitle='Contact Us'/>
+      <form className={style.form}>
+        <Label forInput='username' textLabel='Username:' />
+        <Input type='text' name='username' placeholder='Type your username' />
+        <Label forInput='email' textLabel='E-mail:' />
+        <Input type='text' name='email' placeholder='Type your email' />
+        <Label forInput='questions' textLabel='What do you need?' />
+        <Select name='questions' id='questions'/>
+        <Label forInput='msg' textLabel='Your message:' />
+        <TextArea />
+        <Input type='submit'/>
+      </form>
+  </>
   );
 }
 
--- aula2/ex1/src/components/input/Input.js
+++ aula2/ex1/src/components/input/Input.js
@@ -1,10 +1,19 @@
+import style from './Input.module.css'
+
 const Input = ({type, name, placeholder}) => {
-  return (
-    <input 
-      type={type}
-      name={name}
-      placeholder={placeholder}/>
-  );
+  if(type === 'submit') {
+    return (<input
+      className={`${style.button} ${style.input}`}
+      type={type} />);
+  } else {
+    return (
+      <input
+        className={style.input} 
+        type={type}
+        name={name}
+        placeholder={placeholder}/>
+    );
+  }
 }
 
 export default Input;
